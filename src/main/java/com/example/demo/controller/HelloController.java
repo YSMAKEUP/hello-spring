2e415256e687d4254c0d7843dbc583a70b3152fa
Yunki_Song <rldbs111466@gmail.com>
2025-12-24 16:30:09 +0900

    safawe

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,11 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
     }
+
+    @GetMapping("hell-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false)String name, Model model){
+        model.addAttribute("name", name);
+        return "hello -template";
+    }
+
 }
